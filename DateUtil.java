ac1e1dcf34b8a9de09bebfd4bfa297e231c50135
左玮臣 <verne_zwc@163.com>
2017-09-29 15:16:51 +0800

    增加一些工具类

---
+++ DateUtil.java
@@ -0,0 +1,12 @@
+
+
+/**
+ *
+ * 时间操作类
+ */
+public class DateUtil {
+
+    public static long currentTime(){
+        return System.currentTimeMillis();
+    }
+}
---
+++ RedisHelper.java
@@ -0,0 +1,11 @@
+
+
+/**
+ * 设置缓存工具类
+ */
+public class RedisHelper {
+
+    public static void setBeanToHash(Object object){
+
+    }
+}
