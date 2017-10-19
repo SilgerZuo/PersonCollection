12132329c60bfd33c6b8787ebd8e7dea48542dd5
左玮臣 <verne_zwc@163.com>
2017-10-19 10:29:53 +0800

    增加js的data格式化

---
+++ date.js
@@ -0,0 +1,26 @@
+export function formatDate(dateTo, fmt) {
+  const date = new Date(dateTo * 1000)
+  if (/(y+)/.test(fmt)) {
+    fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length))
+  }
+  const o = {
+    'M+': date.getMonth(),
+    'd+': date.getDate(),
+    'h+': date.getHours(),
+    'm+': date.getMinutes(),
+    's+': date.getSeconds()
+  }
+  for (const k in o) {
+    if (new RegExp(`(${k})`).test(fmt)) {
+      const str = o[k] + ''
+      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? str : padLeftZero(str))
+    }
+  }
+  return fmt
+  // return date
+  // return date.getFullYear()
+}
+
+function padLeftZero(str) {
+  return ('00' + str).substr(str.length)
+}
